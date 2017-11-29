ee173e63f8484692580e2671c0de21181a1d6352
02483564 <umut.boz@kocsistem.com.tr>
2017-11-29 11:38:10 +0300

    commit

--- KS.One.AndroidPlus.Networking/app/src/main/java/com/kocsistem/ksoneandroidplusnetworking/WFM/dal/base/DbWriterDAL.java
+++
@@ -1,9 +0,0 @@
-package com.kocsistem.ksoneandroidplusnetworking.WFM.dal.base;
-
-import android.content.ContentValues;
-
-public interface DbWriterDAL {
-    public  Long insert(ContentValues values, String TABLE_NAME);
-    public  Long update(ContentValues values, String TABLE_NAME, String ColumnName, String Id);
-    public Long delete(String TABLE_NAME, String ColumnName, String id);
-}
